ce6f93bfc34a7780fe7a90b4528dcef0b4292eed
DUSHYANT  JANGRA <dushyant16035@iiitd.ac.in>
2021-10-25 00:33:24 +0530

    Added Export to excel functionality

--- script.js
+++ script.js
@@ -201,8 +201,10 @@ function parseData(){
     //         console.log(element.detail + ' on ' + element.date);
     //     });
     // });
-    var str = `<div style="margin-left:2%"><p style="font-size:20px">No. Of Cards : <b>${cards.size}</b></p><p style="font-size:18px">Cards:-</p></div>
-    <div style="customStyle"><table class="table table-bordered"><tr>
+    var str = `<div style="margin-left:2%"><p style="font-size:20px">No. Of Cards : <b>${cards.size}</b></p>
+    <input type="button" id="btnExport" class="btn btn-success" value="Export Cards to Excel" onclick="Export('cardsTable', 'Trello-Board-Cards')" />
+    <p style="font-size:18px">Cards:-</p></div>
+    <div style="customStyle"><table class="table table-bordered" id="cardsTable"><tr>
     <th>S.No.</th>
     <th>Card Name</th>
     <th>Current List</th>
@@ -242,4 +244,47 @@ function parseData(){
 
     divSummary.innerHTML += `<hr />`;
     
+}
+
+function Export(tableId, filename='Table') {
+    let dataType = 'application/vnd.ms-excel';
+    let extension = '.xlsx';
+
+    let base64 = function(s) {
+        return window.btoa(unescape(encodeURIComponent(s)))
+    };
+
+    let template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
+    let render = function(template, content) {
+        return template.replace(/{(\w+)}/g, function(m, p) { return content[p]; });
+    };
+
+    let tableElement = document.getElementById(tableId);
+
+    let tableExcel = render(template, {
+        worksheet: filename,
+        table: tableElement.innerHTML
+    });
+
+    filename = filename + extension;
+
+    if (navigator.msSaveOrOpenBlob)
+    {
+        let blob = new Blob(
+            [ '\ufeff', tableExcel ],
+            { type: dataType }
+        );
+
+        navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+        let downloadLink = document.createElement("a");
+
+        document.body.appendChild(downloadLink);
+
+        downloadLink.href = 'data:' + dataType + ';base64,' + base64(tableExcel);
+
+        downloadLink.download = filename;
+
+        downloadLink.click();
+    }
 }
